8df31d4be318284336934be64a92c6e9b4f54065
Kakai <kenkai24@yahoo.com>
2022-06-16 21:28:42 +0200

    retrieved movie screenings from API

--- server/server.js
+++ server/server.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT || 5006;
 
 app.use(cors());
 
+const config = {
+    headers: {
+        "client": "STUD_224",
+        "x-api-key": "l2J1IJzLlZ8aW2sXZuNqEg0T2BzLRyO3ESdH8t1b",
+        "authorization": "Basic U1RVRF8yMjRfWFg6emRpRUs1aTJHeTBB",
+        "territory": "XX",
+        "api-version": "v200",
+        "geolocation": "-22.0;14.0",
+        "device-datetime": "2022-06-16T18:06:27.435Z"
+    }
+};
+
 app.get("/api/getMovies/:searchTerm", (req, res) => {
     axios.get(`https://www.omdbapi.com/?apikey=7d212626&s=${req.params.searchTerm}`)
         .then(movies => res.json(movies.data))
@@ -14,6 +26,16 @@ app.get("/api/getMovies/:searchTerm", (req, res) => {
 
 app.get("/api/getById/:id", (req, res) => {
     axios.get(`https://www.omdbapi.com/?apikey=7d212626&i=${req.params.id}&plot=full`)
+        .then(movie => res.json(movie.data))
+})
+
+app.get("/api/movieDetails/:title", (req, res) => {
+    axios.get(`https://api-gate2.movieglu.com/filmLiveSearch/?query=${req.params.title}`, config)
+        .then(movie => res.json(movie.data))
+})
+
+app.get("/api/movieShowtimes?:filmId?:date", (req, res) => {
+    axios.get(`https://api-gate2.movieglu.com/filmShowTimes/?film_id=${req.query.filmId}&date=${req.query.date}`, config)
         .then(movie => res.json(movie.data))
 })
 
