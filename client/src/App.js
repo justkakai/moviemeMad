e9f0c0d1d13d107657c9cc1784ce46e7cd3f40aa
Kakai <kenkai24@yahoo.com>
2022-06-18 02:14:13 +0200

    lay down basic design

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { useState } from 'react';
-import xImage from "./assets/x-image-2.png";
+import { useState, useEffect } from 'react';
+import xImage from "./assets/x-image-3.png";
 import blackBg from "./assets/black-background.png";
 import './App.css';
 
@@ -13,47 +13,9 @@ function App() {
   const [showtimeResults, setShowtimeResults] = useState([]);
   const [modalActive, setModalActive] = useState(false);
 
-  /*   useEffect(() => {
-      axios.get(`/api/movieDetails/${movieTitle}`)
-        .then(movie => {
-          console.log("movieTitle - filmId", movie.data.films[0].film_id);
-          console.log("movieTitle - imdbId", movie.data.films[0].imdb_title_id);
-          if (movie.data.films[0].imdb_title_id === imdbId) {
-            axios.get(`/api/movieShowtimes?filmId=${movie.data.films[0].film_id}&date=2022-06-17`)
-              .then(movie => {
-                setShowtimeResults(movie.data.cinemas);
-                console.log(movie.data.cinemas);
-              })
-          }
-        })
-        .catch(error => console.log("Oops! Looks like we don't have a movie title :("))
-      // eslint-disable-next-line
-    }, [imdbId]) */
-
-  const handleInput = () => {
-    axios.get(`/api/getMovies/${searchWord}`)
-      .then(movies => {
-        console.log("searchword", movies.data)
-        setResults(movies.data.Search)
-      })
-      .catch(error => console.log(error))
-  }
-
-  const handleClick = (id, title) => {
-    console.log("id", id);
-    setImdbId(id);
-    setMovieTitle(title);
-    // setModalActive(true); 
-
-    axios.get(`/api/getById/${id}`)
-      .then(movie => {
-        console.log("getById", movie.data)
-      })
-      .catch(error => console.log(error))
-
+  useEffect(() => {
     axios.get(`/api/movieDetails/${movieTitle}`)
       .then(movie => {
-        // console.log("moviedata", movie.data);
         console.log("movieTitle - filmId", movie.data.films[0].film_id);
         console.log("movieTitle - imdbId", movie.data.films[0].imdb_title_id);
         console.log("this is the current film that's been clicked on", movie.data.films[0].film_name);
@@ -63,14 +25,37 @@ function App() {
               setModalActive(true);
               setShowtimeResults(movie.data.cinemas);
               console.log(movie.data.cinemas);
-              // console.log(movie.data);
             })
-        } else {
-          setShowtimeResults([]);
-          setModalActive(false);
         }
       })
-      .catch(error => console.log("Oops! Looks like we don't have a movie title :("))
+      .catch(error => {
+        setModalActive(false);
+        // setShowtimeResults([]);
+        console.log(error.message, "Oops! Looks like we don't have a movie title :(");
+      })
+    // eslint-disable-next-line
+  }, [imdbId])
+
+  const handleInput = () => {
+    setModalActive(false);
+    axios.get(`/api/getMovies/${searchWord}`)
+      .then(movies => {
+        console.log("searchword", movies.data)
+        setResults(movies.data.Search)
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  const handleClick = (id, title) => {
+    console.log("id", id);
+
+    axios.get(`/api/getById/${id}`)
+      .then((movie) => {
+        setImdbId(id);
+        setMovieTitle(title);
+        console.log("getById", movie.data)
+      })
+      .catch(error => console.log(error.message))
   }
 
   const handleBrokenImg = (image) => {
@@ -79,45 +64,55 @@ function App() {
 
   return (
     <div className="App">
+
+      <h1>movieme<em>Mad!</em></h1>
       <div className='search-section'>
         <input className='search-box' type="text" onChange={(e) => setSearchWord(e.target.value)} value={searchWord} />
         <button className='search-button' type="button" onClick={handleInput}>Search</button>
       </div>
+
       <div
         className='showtimes-container'
         style={modalActive ?
-          { display: "flex" } : { display: "none" }}
-      >
+          { display: "flex" } : { display: "none" }}>
         <button
           className='x-button'
-          onClick={() => setModalActive(false)}>
+          onClick={() => { setModalActive(false); setImdbId("") }}>
           <img src={xImage} alt="x within button to close modal" />
         </button>
         <h2>{movieTitle}</h2>
+
         <ul className='cinemas-showing-film'>
           {showtimeResults.map((cinema) => (
-            <li key={cinema.cinema_id}>
+            <li key={cinema.cinema_id} className="individual-cinema">
               <h3>{cinema.cinema_name}</h3>
-              <ul>
+              <ul className='screening-times'>
                 {cinema.showings.Standard.times.map((time, index) => (
-                  <li key={index}>{time.start_time}</li>
+                  <li key={index}><button className='showtimes-button'>{time.start_time}</button></li>
                 ))}
               </ul>
             </li>
           ))}
         </ul>
+
       </div>
-      <ul>
+
+      <ul className='search-results-ul'>
         {results.map((movie, index) => (
           <li key={index} className="search-results-movie" onClick={() => handleClick(movie.imdbID, movie.Title)}>
             <div className='movie-container'>
+              {/* <p className='result-type'>{movie.Type}</p> */}
               <div className='image-container'><img onError={(e) => handleBrokenImg(e.target)} alt={movie.Title} src={movie.Poster} /></div>
-              <p>{movie.Title}</p>
-              <p>{movie.Year}</p>
+              <p className='movie-title'>{movie.Title}</p>
+              <div className='movie-extra-info'>
+                <p className='result-type'>{movie.Type}!</p>
+                <p className='movie-release-year'>{movie.Year}</p>
+              </div>
             </div>
           </li>
         ))}
       </ul>
+
     </div>
   );
 }
