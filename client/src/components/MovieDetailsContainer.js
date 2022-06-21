4721f6eff38c743ec6674b9a2e626880a201d72f
Kakai <kenkai24@yahoo.com>
2022-06-21 15:35:02 +0200

    added loader for movie details section

--- client/src/components/MovieDetailsContainer.js
+++ client/src/components/MovieDetailsContainer.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-import xImage from '../assets/images/x-image-3.png';
+import xImage from '../assets/images/x-image-4.png';
 import blackBg from "../assets/images/black-background.png";
 import { HiOutlinePlus } from "react-icons/hi";
-import { useEffect, useContext, useRef } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { ModalActiveContext } from '../contexts/ModalActiveContext';
 import { MovieTitleContext } from '../contexts/MovieTitleContext';
 import { ImdbIdContext } from '../contexts/ImdbIdContext';
 import { ShowtimesContext } from '../contexts/ShowtimesContext';
 import { MovieDetailsContext } from '../contexts/MovieDetailsContext';
-// import { IsLoadingContext } from '../contexts/IsLoadingContext';
 import '../styles/MovieDetails.css';
 
 function MovieDetails() {
@@ -20,28 +19,35 @@ function MovieDetails() {
     const { imdbId, setImdbId } = useContext(ImdbIdContext);
     const { movieDetails } = useContext(MovieDetailsContext);
     const { showtimeResults, setShowtimeResults } = useContext(ShowtimesContext);
-    // const { isLoading, setIsLoading } = useContext(IsLoadingContext);
+
+    const [isLoadingScreenings, setIsLoadingScreenings] = useState(false);
+
+
+    let dayOfMonth = Date().split(" ")[2];
+    let monthOfYear = (new Date().getMonth()) + 1;
 
     useEffect(() => {
         scrollRef.current.scrollTo(0, 0);
+        setIsLoadingScreenings(true);
         axios.get(`/api/movieDetails/${movieTitle}`)
             .then(movie => {
                 console.log("movieTitle - filmId", movie.data.films[0].film_id);
                 console.log("movieTitle - imdbId", movie.data.films[0].imdb_title_id);
                 console.log("this is the current film that's been clicked on", movie.data.films[0].film_name);
                 console.log("mmmmm", movie.data);
-                // setIsLoading(true);
                 if (movie.data.films[0].imdb_title_id === imdbId) {
                     setModalActive(true);
-                    axios.get(`/api/movieShowtimes?filmId=${movie.data.films[0].film_id}&date=2022-06-20`)
+                    axios.get(`/api/movieShowtimes?filmId=${movie.data.films[0].film_id}&date=2022-0${monthOfYear}-${dayOfMonth}`)
                         .then(movie => {
-                            // setIsLoading(false);
+                            setIsLoadingScreenings(false);
                             setShowtimeResults(movie.data.cinemas);
                             console.log(movie.data.cinemas);
                         })
+                        .catch(error => console.log(error.message))
                 }
             })
             .catch(error => {
+                setIsLoadingScreenings(false);
                 setShowtimeResults(null);
                 console.log(error.message, "Oops! Looks like we don't have a movie title :(");
             })
@@ -78,22 +84,25 @@ function MovieDetails() {
             </div>
 
             <h3>Screenings in Berlin</h3>
-            {/* <div className='loader' style={isLoading ? { display: "inherit" } : { display: "none" }}></div> */}
             {showtimeResults ?
-                <ul className='cinemas-showing-film'>
-                    {showtimeResults.map((cinema) => (
-                        <li key={cinema.cinema_id} className="individual-cinema">
-                            <h4>{cinema.cinema_name}</h4>
-                            <ul className='screening-times'>
-                                {cinema.showings.Standard.times.map((time, index) => (
-                                    <li key={index}><button className='showtimes-button'>{time.start_time}</button></li>
-                                ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
+                isLoadingScreenings ?
+                    <div className='loader-screenings'></div> :
+                    <ul className='cinemas-showing-film'>
+                        {showtimeResults.map((cinema) => (
+                            <li key={cinema.cinema_id} className="individual-cinema">
+                                <h4>{cinema.cinema_name}</h4>
+                                <ul className='screening-times'>
+                                    {cinema.showings.Standard.times.map((time, index) => (
+                                        <li key={index}><button className='showtimes-button'>{time.start_time}</button></li>
+                                    ))}
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
                 :
-                <p className='no-screenings'>Sorry, there are currently no screenings for this film in Berlin</p>
+                isLoadingScreenings ?
+                    <div className='loader-screenings'></div> :
+                    <p className='no-screenings'>Sorry, there are currently no screenings for {movieTitle} in Berlin</p>
             }
         </section>
     )
